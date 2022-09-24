850e34227a8e2836f999ae936873eced51edea75
Sergej <msergej@mail.ru>
2022-09-24 13:50:29 +0300

    Предварительные результаты на лекции 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World! #2");
+﻿         // Выполнение задааний на лекциях
+         // Лекции, урок 3 (24.09.22)
+
+for(int i=2; i<10; i++) 
+    for(int j=2; j<10; j++)
+        Console.WriteLine($"{i} * {j} = {i * j}");
+
+
+//    string res = Method_04(3, "A");
+//    Console.WriteLine(res);
+//    Console.WriteLine("Hello, World! #2");
+
+
+
+static string Method_04(int count, string c) {
+    int i = 0;
+    string result = string.Empty;
+    while (i < count) {
+        result = result + c;
+        i++;
+    }
+    return result;
+}      
+
